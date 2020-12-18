5c8e7be077da9ec2965cf77d3d0d3c3724e72594
NBJJ <73909448+NBJJ@users.noreply.github.com>
2020-12-18 20:59:43 -0300

    Update Cabina.java

--- Cabina.java
+++ Cabina.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class Peaje extends Vehiculo{
@@ -23,10 +24,10 @@ public class Peaje extends Vehiculo{
 	 
 	}
 	 
-public static Map<Integer, Object> GuardarDatosHashmap(){
+public static Map<Integer, Vehiculo> GuardarDatosHashmap(){
         
-		Vehiculo objetos[] = new Vehiculo[100]; 
-        Map<Integer, Object> mapFileContents = new HashMap<Integer, Object>();
+		Vehiculo objetos[] = new Vehiculo[10]; 
+        Map<Integer, Vehiculo> mapFileContents = new HashMap<Integer, Vehiculo>();
         BufferedReader br = null;
         
         try{
@@ -39,10 +40,12 @@ public static Map<Integer, Object> GuardarDatosHashmap(){
             
             String line = "";
            
-            //lee el txt linea por linea
-            int i = 0 ; 
-            while ( (line = br.readLine()) != null ){
-                
+           
+         
+            
+            while ( (line = br.readLine()) != null){
+            	
+             
                 //Guardar en el arreglo parts todo lo que esté antes del ;
                 String[] parts = line.split(";");
                 
@@ -53,35 +56,32 @@ public static Map<Integer, Object> GuardarDatosHashmap(){
                 Integer turno  = Integer.parseInt( parts[3].trim() );
                 Integer patente = Integer.parseInt(parts[4].trim());
                 Integer tipo = Integer.parseInt(parts[5].trim());
-                Valor atributos = new Valor(ejes,toneladas, marca, patente, turno ,tipo);
-                
-                
+              
                
                 //agrega patente, atributos en HashMap si no están vacías
-                if( !patente.equals("") && !atributos.equals(null) ) {
-                    mapFileContents.put(patente,atributos);
+                   
                if(tipo == 1 ) {
-            	    objetos[i] = new Camion(ejes, toneladas, marca, patente, tipo);
-            	    System.out.println("cid");
-            	    mapFileContents.put(patente, objetos[i]);
-            	    
-         		   //System.out.println(mapFileContents.toString());
-            	//    System.out.println(mapFileContents.get(patente));
-            	  //  System.out.println(objetos[i].toString());
-               }else {
-            	   if(tipo == 2) {
-            		   objetos[i] = new Auto(tipo, toneladas, marca, patente, tipo);
-            		   mapFileContents.put(patente, objetos[i]);
-            		   //System.out.println(mapFileContents.toString());
-            	   }else {
-            		   if(tipo == 3) {
-            			   objetos[i] = new Motocicleta(ejes, toneladas, marca, patente, tipo);
-            		   }
+            	    Camion c = new Camion(ejes, toneladas, marca, patente, tipo);
+            	    mapFileContents.put(patente, c);
+            	    System.out.println("Camion => " + mapFileContents.get(patente));
+            	 
+               }else if
+            	   (tipo == 2) {
+            		   Auto a = new Auto(tipo, toneladas, marca, patente, tipo);
+            		   
+            		   mapFileContents.put(patente, a);
+            		   System.out.println("Auto => " + mapFileContents.get(patente));
+            		   
+            	   }else if(tipo == 3) {
+            			   Motocicleta m = new Motocicleta(ejes, toneladas, marca, patente, tipo);
+            			   mapFileContents.put(patente, m);
+            			   System.out.println("Motocicleta => " + mapFileContents.get(patente));
             	   }
-               }
-                i++; 
             }
-            }                
+       
+               
+            
+                         
         }catch(Exception e){
             e.printStackTrace();
         }finally{
@@ -110,7 +110,7 @@ public int CalcularPeaje(int ejes , int toneladas) {
 		Peaje a = new Peaje(); 
 		System.out.println("\n");
 		System.out.println("Vehiculos del turno "+ turno +":");
-		for(Map.Entry<Integer, Object> entry : a.GuardarDatosHashmap().entrySet()){
+		for(Entry<Integer, Vehiculo> entry : a.GuardarDatosHashmap().entrySet()){
 	        Valor variable = (Valor) entry.getValue();
 			if(variable.getTurno() == turno) {
 				
