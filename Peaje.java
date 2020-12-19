e5bd51aba594165d2c00cd83fac95f51e2412e41
NBJJ <73909448+NBJJ@users.noreply.github.com>
2020-12-19 02:33:31 -0300

    Update Peaje.java

--- Peaje.java
+++ Peaje.java
@@ -1,4 +1,6 @@
 
+
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -6,28 +8,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
 public class Peaje extends Vehiculo{
 	
 	private int valorPeaje;
-	private int Turno;
 	final static String ruta = "C:\\camion1.txt";
+	 static Map<Integer, Vehiculo> mapFileContents = new HashMap<Integer, Vehiculo>();
 	
+	public  Peaje( Map<Integer, Vehiculo> mapFileContents) {
+		this.mapFileContents = mapFileContents; 
+	}
 	
-	
-	public Peaje(int ejes, int toneladas, String marca,int patente, int valorPeaje, int turno, int tipo) {
-		super(ejes,toneladas,marca,patente, tipo);
+	public Peaje(String marca, int ejes, int toneladas,int turno, int patente, int tipo, int mes , int año ) {
+		super(marca , ejes, toneladas, turno, patente , tipo, mes, año);
 		this.valorPeaje = valorPeaje;
-		this.Turno = turno;
+	
 	}
 	public Peaje() {
 	 
 	}
 	 
-public static Map<Integer, Vehiculo> GuardarDatosHashmap(){
+		public static Map<Integer, Vehiculo> GuardarDatosHashmap( ){
         
 		Vehiculo objetos[] = new Vehiculo[10]; 
-        Map<Integer, Vehiculo> mapFileContents = new HashMap<Integer, Vehiculo>();
+       // Map<Integer, Vehiculo> mapFileContents = new HashMap<Integer, Vehiculo>();
         BufferedReader br = null;
         
         try{
@@ -44,8 +47,7 @@ public static Map<Integer, Vehiculo> GuardarDatosHashmap(){
          
             
             while ( (line = br.readLine()) != null){
-            	
-             
+  
                 //Guardar en el arreglo parts todo lo que esté antes del ;
                 String[] parts = line.split(";");
                 
@@ -56,32 +58,30 @@ public static Map<Integer, Vehiculo> GuardarDatosHashmap(){
                 Integer turno  = Integer.parseInt( parts[3].trim() );
                 Integer patente = Integer.parseInt(parts[4].trim());
                 Integer tipo = Integer.parseInt(parts[5].trim());
-              
+                Integer mes = Integer.parseInt(parts[6].trim());
+                Integer año = Integer.parseInt(parts[7].trim());
                
                 //agrega patente, atributos en HashMap si no están vacías
                    
                if(tipo == 1 ) {
-            	    Camion c = new Camion(ejes, toneladas, marca, patente, tipo);
+            	    Camion c = new Camion(marca , ejes, toneladas, turno, patente , tipo, mes, año);
             	    mapFileContents.put(patente, c);
-            	    System.out.println("Camion => " + mapFileContents.get(patente));
+            	   // System.out.println("Camion => " + mapFileContents.get(patente));
             	 
                }else if
             	   (tipo == 2) {
-            		   Auto a = new Auto(tipo, toneladas, marca, patente, tipo);
+            		   Auto a = new Auto(marca, ejes, toneladas, turno, patente, tipo, mes, año);
             		   
             		   mapFileContents.put(patente, a);
-            		   System.out.println("Auto => " + mapFileContents.get(patente));
+            		 //  System.out.println("Auto => " + mapFileContents.get(patente));
             		   
             	   }else if(tipo == 3) {
-            			   Motocicleta m = new Motocicleta(ejes, toneladas, marca, patente, tipo);
+            			   Motocicleta m = new Motocicleta(marca, ejes, toneladas, turno, patente, tipo, mes, año);
             			   mapFileContents.put(patente, m);
-            			   System.out.println("Motocicleta => " + mapFileContents.get(patente));
+            			 //  System.out.println("Motocicleta => " + mapFileContents.get(patente));
             	   }
             }
-       
-               
-            
-                         
+             
         }catch(Exception e){
             e.printStackTrace();
         }finally{
@@ -93,9 +93,7 @@ public static Map<Integer, Vehiculo> GuardarDatosHashmap(){
                 }catch(Exception e){};
             }
         } 
-        
-        
-        
+    
         return mapFileContents;
         
     }
@@ -106,28 +104,60 @@ public int CalcularPeaje(int ejes , int toneladas) {
 }
 
 
-	public void Turnos(  int turno ) {
+	public void Turnos( int turno ) {
 		Peaje a = new Peaje(); 
 		System.out.println("\n");
 		System.out.println("Vehiculos del turno "+ turno +":");
 		for(Entry<Integer, Vehiculo> entry : a.GuardarDatosHashmap().entrySet()){
-	        Valor variable = (Valor) entry.getValue();
-			if(variable.getTurno() == turno) {
+	    
+			if(entry.getValue().getturno() == turno) {
 				
-				System.out.println( entry.getKey() + " => " + variable.getMarca() + " " + variable.getTurno() );
-				
-				
+				System.out.println( entry.getKey() + " => " + entry.getValue().getMarca() + " " + entry.getValue().getturno());		
 		    }
-			
-	    }
-			
-			
+	    }	
 	}
-		
-		public void mostrar() {
-		
+
+	
+		public void mostrarRecaudacion(int turno  ) {
+			Peaje a = new Peaje(); 
+			int sumaTotal=0; 
+			int sumaTotal1=0; 
+			int sumaTotal2=0; 
+			int sumaTotal3=0; 
+			for(Entry<Integer, Vehiculo> entry : a.GuardarDatosHashmap().entrySet()){
+				
+				Auto a1 = new Auto();
+				Camion c1 = new Camion();
+				Motocicleta m1 = new Motocicleta();
+				
+				
+				if(entry.getValue().getturno() == turno) {
+					for(Entry<Integer, Vehiculo> entry2 : a.GuardarDatosHashmap().entrySet()){
+				if(entry.getValue().getTipo()== 1 ) { //Camion
+					sumaTotal1 = sumaTotal1 + c1.CalcularPeaje(entry.getValue().getEjes(), entry.getValue().getToneladas()) ;
+					System.out.println(sumaTotal1);
+				}else if(entry.getValue().getTipo()==2) { //Auto
+					sumaTotal2 = a1.CalcularPeaje() + sumaTotal2; 
+					System.out.println(sumaTotal2);
+				}else if(entry.getValue().getTipo()==3) {//Motocicleta 
+					sumaTotal3 = m1.CalcularPeaje() + sumaTotal3;
+					
+					System.out.println(sumaTotal3);
+				}
+			}
+				
 		}
-		
+			System.out.println(sumaTotal1);
+			System.out.println(sumaTotal2);
+			System.out.println(sumaTotal3);
+			 sumaTotal= (sumaTotal1 + sumaTotal2 + sumaTotal3) ; 
+			System.out.println("Suma Total turno "+ turno + " es : $"+sumaTotal);
+	}
+		}
 }
 			
+
+
+
+
 		
