f5fb6b76f48161d2fe63669b8b5ee3c79b02c476
NBJJ <73909448+NBJJ@users.noreply.github.com>
2020-12-19 02:30:52 -0300

    Update Cabina.java

--- Cabina.java
+++ Cabina.java
@@ -1,18 +1,18 @@
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.Map.Entry;
 
 //MAIN DEL PROYECTO
 
 public class Cabina {
 	public static void main(String arg[]){
-	
+		
 		//Creamos un objeto peaje para enviar los parámetros iniciales 
 		Peaje peaje1 = new Peaje();
-		
+		peaje1.Turnos(3);
+		peaje1.mostrarRecaudacion(1);
 		peaje1.GuardarDatosHashmap();
 
+		
 	}	
 	
 }
